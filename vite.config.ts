5a1c364aa9a8101c63a51d84c10ec1f2bd5ece25
zyjyj8 <1971210410@qq.com>
2026-03-16 19:19:46 +0800

    Update vite.config.ts
    添加base配置，修复GitHub Pages空白页

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pindouwangzhan/', // 关键配置，和仓库名一致
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
